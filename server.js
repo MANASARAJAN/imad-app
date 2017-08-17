0dc9d8a3993d59b56bf777c97f9971463701451c
MANASARAJAN <30653827+MANASARAJAN@users.noreply.github.com>
2017-08-18 02:08:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-   res.send(create template(articleone));
+   res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
