7ad2741a17f53206be4ee75fd2f5a147a2c277c5
MANASARAJAN <30653827+MANASARAJAN@users.noreply.github.com>
2017-08-18 04:28:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ element.innerHTML ='New Value' ;
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 10);
 };
