90c473bd4811e740c09e76d53250f9c5a2dcd067
MANASARAJAN <30653827+MANASARAJAN@users.noreply.github.com>
2017-08-19 05:42:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,6 @@ button.onclick = function () {
     // make the request
      request.open('GET', 'http://manasarajan17.imad.hasura-app.io/counter', true);
      request.send(null);
-     console.log('EXECUTED');
 };
 
 //submit name
